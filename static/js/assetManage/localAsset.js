e72153861821732d60daed95ca5a0450e44c9144
Chris-debug-0225 <1343569362@qq.com>
2021-06-25 23:14:09 +0800

    动态资产管理部分完成

--- static/js/assetManage/localAsset.js
+++ static/js/assetManage/localAsset.js
@@ -1,9 +1,6 @@
 var LocalChart = echarts.init(document.getElementById('Local'));
 
 var data = [];
-for (let i = 0; i < 23; ++i) {
-    data.push(Math.round(Math.random() * 200));
-}
 
 var LocalOption = {
     xAxis: {
@@ -18,24 +15,43 @@ var LocalOption = {
         axisLabel:{
             color: 'white'
         },
-        data: ['北京','天津','上海','重庆','河北','山西','辽宁','吉林','黑龙江','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','海南','四川','贵州','云南'],
+        // data: ['北京','天津','上海','重庆','河北','山西','辽宁','吉林','黑龙江','江苏','浙江','安徽','福建','江西','山东','河南','湖北','湖南','广东','海南','四川','贵州','云南'],
+        data: [],
         inverse: true,
         animationDuration: 300,
         animationDurationUpdate: 300,
         max: 23// only the largest 3 bars will be displayed
     },
+    // grid: {
+    //     left: '0%',
+    //     right: '0%',
+    //     top: '0%',
+    //     bottom: '0%',
+    //     containLabel: true
+    // },
     series: [{
         realtimeSort: true,
         name: 'X',
-        itemStyle:{
-            color:'#98FB98'
-                  },
+        color: 'white',
+        itemStyle: {
+                color: new echarts.graphic.LinearGradient(
+                    0, 0, 0, 1,
+                    [
+                        {offset: 0, color: '#76EEC6'},
+                        {offset: 0.5, color: '#B0E2FF'},
+                        {offset: 1, color: '#4F4F4F'}
+                    ]
+                )
+        },
         type: 'bar',
         data: data,
         label: {
             show: true,
             position: 'right',
             valueAnimation: true,
+            textStyle:{
+                color:'#FFFFFF',
+            }
         }
     }],
     legend: {
@@ -47,24 +63,6 @@ var LocalOption = {
     animationEasingUpdate: 'linear'
 };
 
-function run () {
-    var data = LocalOption.series[0].data;
-    for (var i = 0; i < data.length; ++i) {
-        if (Math.random() > 0.9) {
-            data[i] += Math.round(Math.random() * 2000);
-        }
-        else {
-            data[i] += Math.round(Math.random() * 200);
-        }
-    }
-    LocalChart.setOption(LocalOption);
-}
 
-setTimeout(function() {
-    run();
-}, 0);
-setInterval(function () {
-    run();
-}, 3000);
 
 LocalChart.setOption(LocalOption);
