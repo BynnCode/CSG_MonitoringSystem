cfd5af23d657fb08fcf6b663610e61cadc9fbcb9
bynn <bynn>
2021-09-19 09:45:41 +0800

    优化了项目结构

--- apps/__init__.py
+++ apps/__init__.py
@@ -1,17 +1,20 @@
 # -*- codeing = utf-8 -*-
 from flask import Flask
 from apps import settings
-from apps.asset import asset_bp
+from apps.asset.asset import asset_bp
 from apps.controller import utils
-from apps.flow import flow_bp
-from apps.log import log_bp
-from apps.monitor import monitor_bp
-from apps.suricata import suricata_bp
+from apps.flow.flow import flow_bp
+from apps.log.log import log_bp
+from apps.monitor.monitor import monitor_bp
+from apps.suricata.suricata import suricata_bp
+from flask_cors import CORS
 
 
 def create_app():
     app = Flask(__name__,template_folder='../templates',static_folder='../static')  #flask创建对象
+    CORS(app,supports_credentials=True)
     app.config.from_object(settings)    #加载配置
+    # app.config.from_pyfile('settings.py')
 
     # 注册蓝图
     app.register_blueprint(flow_bp)
